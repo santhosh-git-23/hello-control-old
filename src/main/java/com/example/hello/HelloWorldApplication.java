c6d4cef222dad95742b63493511effae9112d863
santhosh-git-23 <129048391+santhosh-git-23@users.noreply.github.com>
2023-06-02 11:56:06 +0530

    Update HelloWorldApplication.java

--- src/main/java/com/example/hello/HelloWorldApplication.java
+++ src/main/java/com/example/hello/HelloWorldApplication.java
@@ -3,7 +3,9 @@ package com.example.hello;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
+
 @SpringBootApplication
+@ComponentScan(basePackages = "com.example.hello")
 public class HelloWorldApplication {
 
 	public static void main(String[] args) {
